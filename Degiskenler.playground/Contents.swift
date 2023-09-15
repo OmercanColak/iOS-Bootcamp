c9378cec83bb36ee508a6f40ca247f75dfac5079
OmercanColak <omercancolak06@gmail.com>
2023-09-15 16:24:08 +0300

    Standart Programlama | Added

--- Degiskenler.playground/Contents.swift
+++ Degiskenler.playground/Contents.swift
@@ -25,3 +25,41 @@ print("Ürün Adı: \(urun_adi)")
 print("Ürün Adet: \(urun_adet)")
 print("Ürün Fiyat: \(urun_fiyat) ₺")
 print("Ürün Tedarikçi: \(urun_tedarikci)")
+
+
+
+// Constant - Sabitler
+// kotlin - val , java - final , dart - const
+// swift - let
+
+var sayi = 10
+print(sayi)
+sayi = 20
+print(sayi)
+
+let numara = 100
+print(numara)
+
+// Tür Dönüşümü - Type Casting
+// Sayısaldan - Sayısala
+var i = 42
+var d = 56.64
+
+var sonuc1 = Double(i)
+var sonuc2 = Int(d)
+print(sonuc1)
+print(sonuc2)
+
+//Sayısaldan Metine
+var sonuc3 = String(i)
+var sonuc4 = String(d)
+print(sonuc3)
+print(sonuc4)
+
+//Metinden Sayısala
+var yazi = "34."
+if let sonuc5 = Int(yazi) { //Sonucun optional olmaması için
+    print(sonuc5)
+}else{
+    print("Dönüşüm hatası")
+}
