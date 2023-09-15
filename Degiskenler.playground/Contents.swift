e5ac6eb8a52705bf0048dd4298d2ee41baf1eab6
OmercanColak <omercancolak06@gmail.com>
2023-09-15 12:55:11 +0300

    Ödev-1 | Eklendi

---
+++ Degiskenler.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+print("Hello World")
+
+var ogrenciAdi = "Ahmet"
+var ogrenciYas = 22
+var ogrenciBoy = 1.78
+var ogrenciBasHarf = "A"
+var ogrenciDevamEdiyormu = true
+
+print(ogrenciAdi)
+print(ogrenciYas)
+print(ogrenciBoy)
+print(ogrenciBasHarf)
+print(ogrenciDevamEdiyormu)
+
+
+var urun_id:Int = 3416
+var urun_adi:String = "Macbook Pro"
+var urun_adet:Int = 100
+var urun_fiyat:Int = 34999
+var urun_tedarikci:String = "Apple"
+
+print("Ürün ID: \(urun_id)")
+print("Ürün Adı: \(urun_adi)")
+print("Ürün Adet: \(urun_adet)")
+print("Ürün Fiyat: \(urun_fiyat) ₺")
+print("Ürün Tedarikçi: \(urun_tedarikci)")
---
+++ Odev-1.playground/Contents.swift
@@ -0,0 +1,100 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var ilce = "Çankaya"
+var kita = "Asya"
+var faks = "+90-312-3102416"
+var postaKodu:Int = 06320
+var instagramAdresi = "@omercancolak"
+var calistiginBolum = "Yazılım"
+var urun_miktarı:Int = 236
+var musteri_soyadi = "Urgancı"
+var odeme_mikari:Int = 23333
+var dogum_tarihi = "17-05-1997"
+var borc:Int = 132
+var medeni_Hali = "Bekar"
+var video_yorumu = "Bu bootcamp bana çok yararlı oldu."
+var odeme_saati = 21.21
+var eft_miktari:Int = 2341
+var satilan_miktar:Int = 221
+var telefon_modeli = "Iphone 14 Pro"
+var dergi_adi = "Fanatik"
+var basim_tarihi = "12.02.2023"
+var zam_miktari = 1231
+var daire_sayisi:Int = 23
+var enlem:Double = 22.121
+var boylam:Double = 22.232
+var yemek_adi = "Karnıyarık"
+var urun_fiyat:Int = 65
+var sirket = "Trendyol"
+var video_adi = "Hello Youtube"
+var muzik_suresi:Double = 4.21
+var mekan_puani:Double = 4.73
+var dosya_adi = "iOS-Bootcamp"
+var resim_formati = ".jpg"
+var renk = "Kırmızı"
+var renkKodu = "#FF0000"
+var bilgisayar_modeli = "Macbook Pro"
+var ekran_boyutu = "15"
+var kullanim_suresi:Double = 24.12
+var basinc = 12.332
+var etkinlik_gunu = "12.10.2023"
+var odeme_gunu = "25.09.2023"
+var yolculuk_cikis_tarihi = "12.09.2023"
+var mahalle_adi = "Bostancık"
+var otobus_hatti = 322
+var kullanilan_dakika = 2.23
+var kargo_takip_no:Int = 1231263
+var kupon_suresi = 65
+var kupon_kodu = "12as123tm347"
+var fatura_tarihi = "12.04.2023"
+
+print("İlçe : \(ilce)")
+print("Kıta : \(kita)")
+print("Faks : \(faks)")
+print("Posta Kodu : \(postaKodu)")
+print("Instagram Adresi : \(instagramAdresi)")
+print("Calıştığın Bölüm : \(calistiginBolum)")
+print("Ürün Miktari : \(urun_miktarı) Adet")
+print("Müşterinin Soyadı: \(musteri_soyadi)")
+print("Ödeme Miktarı : \(odeme_mikari)")
+print("Doğum Tarihi : \(dogum_tarihi)")
+print("Borç : \(borc) ₺")
+print("Medeni Hali : \(medeni_Hali)")
+print("Video Yorumu : \(video_yorumu)")
+print("Ödeme Saati : \(odeme_saati)")
+print("EFT Miktarı : \(eft_miktari) ₺")
+print("Satılan Miktar : \(satilan_miktar)")
+print("Telefon Modeli : \(telefon_modeli)")
+print("Dergi Adı : \(dergi_adi)")
+print("Basım Tarihi : \(basim_tarihi)")
+print("Zam Miktarı : \(zam_miktari)")
+print("Daire Sayısı : \(daire_sayisi)")
+print("Enlem : \(enlem)")
+print("Boylam : \(boylam)")
+print("Yemek Adı : \(yemek_adi)")
+print("Ürün Fiyatı : \(urun_fiyat)")
+print("Şirket : \(sirket)")
+print("Video Adı : \(video_adi)")
+print("Müzik Süresi : \(muzik_suresi)")
+print("Mekan Puanı : \(mekan_puani)")
+print("Dosya Adı : \(dosya_adi)")
+print("Resim Formatı : \(resim_formati)")
+print("Renk : \(renk)")
+print("Renk Kodu : \(renkKodu)")
+print("Bilgisayar Modeli : \(bilgisayar_modeli)")
+print("Ekran Boyutu : \(ekran_boyutu)")
+print("Kullanım Süresi : \(kullanim_suresi)")
+print("Basınç : \(basinc)")
+print("Etkinlik Günü : \(etkinlik_gunu)")
+print("Ödeme Günü : \(odeme_gunu)")
+print("Yolculuk Çıkış Tarihi : \(yolculuk_cikis_tarihi)")
+print("Mahalle Adı : \(mahalle_adi)")
+print("Otobüs Hattı : \(otobus_hatti)")
+print("Kullanılan Dakika : \(kullanilan_dakika)")
+print("Kargo Takip No : \(kargo_takip_no)")
+print("Kupon Süresi : \(kupon_suresi) Gün")
+print("Kupon Kodu : \(kupon_kodu)")
+print("Fatura Tarihi : \(fatura_tarihi)")
+
