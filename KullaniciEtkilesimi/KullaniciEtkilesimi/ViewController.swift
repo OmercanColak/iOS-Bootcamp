5c4922b878a6fb13afcd4853d58be3563aeb1b86
Ömercan Çolak <omercancolak06@gmail.com>
2023-11-21 16:19:11 +0300

    AlertAction | Added

---
+++ KullaniciEtkilesimi/KullaniciEtkilesimi/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  KullaniciEtkilesimi
+//
+//  Created by Ömercan Çolak on 21.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonAlert(_ sender: Any) {
+        let alertController = UIAlertController(title: "Başlık", message: "Mesaj", preferredStyle: .alert)
+        
+        let iptalAction = UIAlertAction(title: "İptal", style: .cancel) {action in
+            print("İptal Seçildi")
+        }
+        alertController.addAction(iptalAction)
+        let tamamAction = UIAlertAction(title: "Tamam", style: .destructive) {action in
+            print("Tamam Seçildi")
+        }
+        
+        alertController.addAction(tamamAction)
+        
+        self.present(alertController, animated: true)
+        
+    }
+    
+    @IBAction func buttonActionSheet(_ sender: Any) {
+        let alertController = UIAlertController(title: "Başlık", message: "Mesaj", preferredStyle: .actionSheet)
+        
+        let iptalAction = UIAlertAction(title: "İptal", style: .cancel) {action in
+            print("İptal Seçildi")
+        }
+        alertController.addAction(iptalAction)
+        let tamamAction = UIAlertAction(title: "Tamam", style: .destructive) {action in
+            print("Tamam Seçildi")
+        }
+        
+        alertController.addAction(tamamAction)
+        
+        self.present(alertController, animated: true)
+    }
+    
+    @IBAction func buttonOzelAlert(_ sender: Any) {
+        let alertController = UIAlertController(title: "Başlık", message: "Mesaj", preferredStyle: .alert)
+        alertController.addTextField{ textField in
+            textField.placeholder = "Veri Giriniz"
+            textField.keyboardType = .numberPad
+            textField.isSecureTextEntry = true
+            
+        }
+        
+        let tamamAction = UIAlertAction(title: "Kaydet", style: .cancel) {action in
+            let tf = alertController.textFields![0] as UITextField
+            let tf1 = alertController.textFields![1] as UITextField
+            if let alinanVeri = tf.text , let alinanVeri1 = tf1.text {
+                print("Veri : \(alinanVeri) - \(alinanVeri1)")
+            }
+            print("Tamam Seçildi")
+        }
+        
+        alertController.addAction(tamamAction)
+        
+        self.present(alertController, animated: true)
+    }
+    
+}
+
