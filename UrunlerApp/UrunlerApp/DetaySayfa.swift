4068f04959c6a9cdd9aad7ae37f96c14f35a96df
Ömercan Çolak <omercancolak06@gmail.com>
2024-02-26 15:45:30 +0300

    Urunler | Added

---
+++ UrunlerApp/UrunlerApp/DetaySayfa.swift
@@ -0,0 +1,34 @@
+//
+//  DetaySayfa.swift
+//  UrunlerApp
+//
+//  Created by Ömercan Çolak on 26.02.2024.
+//
+
+import UIKit
+
+class DetaySayfa: UIViewController {
+
+    @IBOutlet weak var imageViewUrun: UIImageView!
+    @IBOutlet weak var labelUrunFiyat: UILabel!
+    var urun:Urunler?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        if let u = urun {
+            self.navigationItem.title = u.ad
+            imageViewUrun.image = UIImage(named: u.resim!)
+            labelUrunFiyat.text = "\(u.fiyat!) ₺"
+        }
+        
+
+    }
+    
+    @IBAction func buttonSepeteEkle(_ sender: Any) {
+        if let u = urun {
+            print("Detay Sayfa: \(u.ad) sepete Eklendi!")
+        }
+    }
+    
+}
