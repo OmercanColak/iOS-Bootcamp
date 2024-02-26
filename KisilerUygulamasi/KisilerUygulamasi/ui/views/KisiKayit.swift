d53b78ac68e8da1f8de207df68c21e1344e32c1f
Ömercan Çolak <omercancolak06@gmail.com>
2024-02-26 12:16:51 +0300

    KisilerUygulamasi
    Added

---
+++ KisilerUygulamasi/KisilerUygulamasi/ui/views/KisiKayit.swift
@@ -0,0 +1,33 @@
+//
+//  KisiKayit.swift
+//  KisilerUygulamasi
+//
+//  Created by Ömercan Çolak on 8.02.2024.
+//
+
+import UIKit
+
+class KisiKayit: UIViewController {
+    
+    @IBOutlet weak var tfKisiAd: UITextField!
+    
+    @IBOutlet weak var tfKisiTel: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    @IBAction func buttonKaydet(_ sender: Any) {
+        if let ka = tfKisiAd.text, let kt = tfKisiTel.text {
+            kaydet(kisi_ad: ka, kisi_tel: kt)
+        }
+        
+    }
+    
+    func kaydet(kisi_ad:String,kisi_tel:String){
+        print("Kişi Kaydet : \(kisi_ad) - \(kisi_tel)")
+    }
+}
